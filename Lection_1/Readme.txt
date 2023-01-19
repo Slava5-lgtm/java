430110d4d6dcb2f7ee1a7724bb157dcfe10e0d13
Vaceslav <vaceslavsergunin@gmail.com>
2023-01-19 21:40:46 +0600

    добавил папки

---
+++ Lection_1/Readme.txt
@@ -0,0 +1,9 @@
+Настройка рабочего места
+Шаг 1:
+Java JDK https://www.oracle.com/java/technologies/downloads/
+Шаг 2:
+Extension Pack VS Code
+https://marketplace.visualstudio.com/items?itemName=vscjava.vscode-javapack
+При желаниии:
+https://www.jetbrains.com/ru-ru/idea/
+println(“Hello world”);
---
+++ Lection_1/introductions.java
@@ -0,0 +1,10 @@
+package Lection_1;
+
+public class introductions {
+    public static void main(String[] args) {
+        /**System.out.println("вывести в кансоль и нажать тругольник в верхний правый угол");*/
+        System.out.println("или начать наберать sys и она автомотически появиться");
+        // Также можно окрыть в строеном терминале и прописать нзвания файла java introductions.java
+    }
+    
+}
