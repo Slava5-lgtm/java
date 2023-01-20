7c7d9cf7528ce273b5ac437a48bb3a90ccc00f89
Vaceslav <vaceslavsergunin@gmail.com>
2023-01-21 03:15:48 +0600

    добавил файл

---
+++ Lection_1/program.java
@@ -0,0 +1,11 @@
+package Lection_1;
+
+public class program {
+    public static void main(String[] args) {
+        int m =123;
+        System.out.println(--m);
+        System.out.println(m);
+
+    }
+    
+}
