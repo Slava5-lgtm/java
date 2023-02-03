15d041eaf8bf100e53f95ab8e435605160925384
Vaceslav <vaceslavsergunin@gmail.com>
2023-02-03 19:44:55 +0600

    +

---
+++ Lesson_4/dz.java
@@ -0,0 +1,56 @@
+package Lesson_4;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import static java.util.Collections.max;
+import static java.util.Collections.min;
+public class dz {
+    public static void main(String[] args) {
+        // Пусть дан произвольный список целых чисел
+        // Нужно удалить из него четные числа
+        // Найти минимальное значение
+        // Найти максимальное значение
+        // Найти среднее значение
+        Random rnd = new Random();
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            int val = rnd.nextInt(-100, 100);
+            list1.add(val);
+        }
+        System.out.printf("Первоначальный список %s\n", list1);
+        int max = max(list1);
+        int min = min(list1);
+        int maxItem = list1.get(0);
+        int minItem = list1.get(0);
+        int sumItems = 0;
+        for (int item : list1) {
+            if (item > maxItem) {
+                maxItem = item;
+            }
+            if (item < minItem) {
+                minItem = item;
+            }
+            sumItems += item;
+        }
+        int i = 0;
+        while (i < list1.size()) {
+            if (list1.get(i) % 2 == 0) {
+                list1.remove(i);
+            }
+            else {
+                i++;
+                }
+        }
+        float average = (float) sumItems / list1.size();
+        System.out.printf("Список с удаленными четными числами %s", list1);
+        
+        System.out.printf("Максимальный элемент при помощи max %s\n", max);
+        System.out.printf("Максимальный элемент %s\n", maxItem);
+        System.out.printf("Минимальный элемент при помощи min %s\n", min);
+        System.out.printf("Минимальный элемент %s\n", minItem);
+        System.out.printf("Сумма элементов %s\n", sumItems);
+        System.out.printf("Среднее арифметическое %s\n", average);
+    }
+    }
+
