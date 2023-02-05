efdb075bc6d0528233b887d2186473d6b344763a
Vaceslav <vaceslavsergunin@gmail.com>
2023-02-05 10:21:50 +0600

    +

--- Lesson_6/HashSet.java
+++ Lesson_6/HashSet.java
@@ -6,12 +6,12 @@
 //  clear()–удаление всех элементов коллекции.
 //  size()–возвращает количество элементов коллекции.
 
-package Lesson_6;
+//package Lesson_6{
 
 // import java.util.Arrays;
 // import java.util.Set;
 
-//public class HashSet {
+// public class HashSet {
 //     public static void main(String[] args) {
 //         Set<Integer> set = new HashSet<>(); // конструктор
 //         set.add(1); // добовляем
@@ -58,6 +58,31 @@ package Lesson_6;
 //         System.out.println(r); // [2, 3, 5, 7]
 //         System.out.println(s); // [1, 4, 6]
 //     }
+
+
+// public class Lesson_6 {
+// import java.util.Arrays;
+// import java.util.TreeSet;
+    
+//     public class HashSet{
+//     // first()
+//     // last()
+//     // headSet(E)
+//     // tailSet(E)
+//     // subSet(E1,E2)
+//         public static void main(String[] args) {
+//             var a = new TreeSet<>(Arrays.asList(1, 7, 2, 3, 6, 4, 5));
+//             var b = new TreeSet<>(Arrays.asList(13, 3, 17, 7, 2, 11, 5));
+//             System.out.println(a); // [1, 2, 3, 4, 5, 6, 7]
+//             System.out.println(b); // [2, 3, 5, 7, 11, 13, 17]
+//             System.out.println(a.headSet(4)); // [1, 2, 3]
+//             System.out.println(a.tailSet(4)); // [4, 5, 6, 7]
+//             System.out.println(a.subSet(3, 5)); // [3, 4] 
+//         }
+//     }
 // }
+
+
+    
 
 
--- Lesson_6/LinkedHashSet.java
+++ Lesson_6/LinkedHashSet.java
@@ -25,5 +25,3 @@ package Lesson_6;
 //     }
 
 // }
-
-
